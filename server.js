f307c731a7bca21f1e06e215dff2223230edb85d
sayedsana <31034110+sayedsana@users.noreply.github.com>
2017-09-19 21:06:18 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -33,10 +33,7 @@ app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
 
-app.get('/main.js',function(req,res)
-{
-   res.sendFile(path.join(__dirname, 'ui', 'main.js')); 
-});
+
 
 
 // Do not change port, otherwise your app won't run on IMAD servers
