207fd08b405eeea9ba87f257e35b4647e9747ac7
sayedsana <31034110+sayedsana@users.noreply.github.com>
2017-09-19 22:25:41 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -4,7 +4,16 @@ var element= document.getElementById('main-text');
 element.innerHTML ='MY VALUES';
 
 var img=document.getElementById('madi');
+var marginleft=0;
+function moveRight()
+{
+    marginleft=marginleft+10;
+    img.style.marginLeft=marginleft+'px';
+}
+
+
+
 img.onclick=function()
 {
-  img.style.marginLeft='100px';  
+  var interval=setInterval(moveRight,50);
 };
